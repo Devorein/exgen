dde0bcc07cd43c0a7454511b48e4ff098cc2957f
Safwan Shaheer <devorein00@gmail.com>
2022-01-17 17:22:56 +0600

    Downgraded mdast and micromark versions for cjs

--- libs/extractExamples.ts
+++ libs/extractExamples.ts
@@ -1,158 +1,224 @@
 import fs from 'fs/promises';
 import path from 'path';
-import ts, { ArrowFunction, Block, CallExpression, ExpressionStatement, Identifier, ImportDeclaration, NamedImportBindings, PropertyAccessExpression, StringLiteral, VariableDeclaration, VariableDeclarationList, VariableStatement } from 'typescript';
+import ts, {
+	ArrowFunction,
+	Block,
+	CallExpression,
+	ExpressionStatement,
+	Identifier,
+	ImportDeclaration,
+	NamedImportBindings,
+	PropertyAccessExpression,
+	StringLiteral,
+	VariableDeclaration,
+	VariableDeclarationList,
+	VariableStatement,
+} from 'typescript';
 import { ExampleInfo, FunctionExampleRecord } from './types';
 
 function functionChecker(expressionStatement: ExpressionStatement, functionName: string) {
-  function checker(functionCallExpressions: CallExpression[]): CallExpression[] | null {
-    const lastCallExpression = functionCallExpressions[functionCallExpressions.length - 1];
+	function checker(functionCallExpressions: CallExpression[]): CallExpression[] | null {
+		const lastCallExpression = functionCallExpressions[functionCallExpressions.length - 1];
 
-    if (lastCallExpression.kind === 207) {
-      const identifier = lastCallExpression.expression as Identifier | PropertyAccessExpression;
-      if (identifier.kind === 79 && identifier.escapedText === functionName) {
-        return functionCallExpressions
-      }
-      // For property access expression
-      else if (identifier.kind === 205) {
-        return checker(functionCallExpressions.concat(identifier.expression as CallExpression))
-      }
-    }
-    return null;
-  }
+		if (lastCallExpression.kind === 207) {
+			const identifier = lastCallExpression.expression as Identifier | PropertyAccessExpression;
+			if (identifier.kind === 79 && identifier.escapedText === functionName) {
+				return functionCallExpressions;
+			}
+			// For property access expression
+			else if (identifier.kind === 205) {
+				return checker(functionCallExpressions.concat(identifier.expression as CallExpression));
+			}
+		}
+		return null;
+	}
 
-  if (expressionStatement.kind === 237) {
-    const functionExpression = expressionStatement.expression as CallExpression;
-    return checker([functionExpression])
-  }
-  return null;
+	if (expressionStatement.kind === 237) {
+		const functionExpression = expressionStatement.expression as CallExpression;
+		return checker([functionExpression]);
+	}
+	return null;
 }
 
 // Check if the first argument is a string and second argument is an anonymous function expression
-function argumentsChecker(callExpression: ts.CallExpression, cb: (stringArgument: StringLiteral, arrowFunctionArgument: ArrowFunction) => void) {
-  const stringLiteral = callExpression.arguments[0] as StringLiteral;
-  if (stringLiteral.kind === 10 || stringLiteral.kind === 14) {
-    const arrowFunction = callExpression.arguments[1] as ArrowFunction;
-    if (arrowFunction.kind === 213) {
-      cb(stringLiteral, arrowFunction)
-    }
-  }
+function argumentsChecker(
+	callExpression: ts.CallExpression,
+	cb: (stringArgument: StringLiteral, arrowFunctionArgument: ArrowFunction) => void
+) {
+	const stringLiteral = callExpression.arguments[0] as StringLiteral;
+	if (stringLiteral.kind === 10 || stringLiteral.kind === 14) {
+		const arrowFunction = callExpression.arguments[1] as ArrowFunction;
+		if (arrowFunction.kind === 213) {
+			cb(stringLiteral, arrowFunction);
+		}
+	}
 }
 
 // Check if a variable is equal to the passed identifier
-export function variableChecker(variableStatement: VariableStatement, variableName: string, cb: (initializer: ts.Expression) => void) {
-  if (variableStatement.kind === 236) {
-    const variableDeclarationList = variableStatement.declarationList as VariableDeclarationList;
-    if (variableDeclarationList.kind === 254) {
-      const variableDeclaration = variableDeclarationList.declarations[0] as VariableDeclaration;
-      if (variableDeclaration.kind === 253) {
-        const identifier = variableDeclaration.name as Identifier;
-        if (identifier.escapedText === variableName && variableDeclaration.initializer) {
-          cb(variableDeclaration.initializer)
-        }
-      }
-    }
-  }
+export function variableChecker(
+	variableStatement: VariableStatement,
+	variableName: string,
+	cb: (initializer: ts.Expression) => void
+) {
+	if (variableStatement.kind === 236) {
+		const variableDeclarationList = variableStatement.declarationList as VariableDeclarationList;
+		if (variableDeclarationList.kind === 254) {
+			const variableDeclaration = variableDeclarationList.declarations[0] as VariableDeclaration;
+			if (variableDeclaration.kind === 253) {
+				const identifier = variableDeclaration.name as Identifier;
+				if (identifier.escapedText === variableName && variableDeclaration.initializer) {
+					cb(variableDeclaration.initializer);
+				}
+			}
+		}
+	}
 }
 
 export async function extractExamples(testFilesDirectory: string) {
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-  const functionExamplesRecord: FunctionExampleRecord = {};
+	const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+	const functionExamplesRecord: FunctionExampleRecord = {};
 
-  async function traverse(testFilesDirectory: string) {
-    const testFiles = await fs.readdir(testFilesDirectory);
-    for (let index = 0; index < testFiles.length; index++) {
-      const testFile = testFiles[index];
-      const testFilePath = path.join(testFilesDirectory, testFile);
-      const fsStat = await fs.stat(testFilePath);
-      if (fsStat.isDirectory()) {
-        // If its a directory, recursively call the function
-        await traverse(testFilePath)
-      } else {
-        // Make sure its a test file
-        if (testFile.endsWith('test.ts')) {
-          const program = ts.createProgram([testFilePath], {});
-          const sourceFile = program.getSourceFile(testFilePath)!;
-          const importedFunctions: Set<string> = new Set();
-          for (let index = 0; index < sourceFile.statements.length; index++) {
-            const statement = sourceFile.statements[index] as ExpressionStatement | ImportDeclaration;
-            if (statement.kind === 265) {
-              if (statement.importClause) {
-                const namedImports = statement.importClause.namedBindings as NamedImportBindings;
-                if (namedImports.kind === 268) {
-                  const importSpecifiers = namedImports.elements;
-                  importSpecifiers.forEach(importSpecifier => {
-                    importedFunctions.add(importSpecifier.name.escapedText as string)
-                  })
-                }
-              }
-            } else {
-              const describeFunctionStatement = functionChecker(statement, "describe");
-              if (describeFunctionStatement) {
-                argumentsChecker(describeFunctionStatement[0], (describeFunctionFirstArgument, describeFunctionSecondArgument) => {
-                  const functionName = describeFunctionFirstArgument.text;
-                  // Make sure the function has been imported in the module
-                  if (importedFunctions.has(functionName)) {
-                    
-                    const describeFunctionBlock = describeFunctionSecondArgument.body as Block;
-                    if (describeFunctionBlock.kind === 234) {
-                      // Loop through all the function block statements
-                      for (let index = 0; index < describeFunctionBlock.statements.length; index++) {
-                        const describeFunctionBlockStatement = describeFunctionBlock.statements[index] as ExpressionStatement;
-                        const itFunctionStatement = functionChecker(describeFunctionBlockStatement, "it");
-    
-                        if (itFunctionStatement) {
-                          // Only if the first argument of it and describe are the same
-                          argumentsChecker(itFunctionStatement[0], (itFunctionFirstArgument, itFunctionSecondArgument) => {
-                            const exampleMessage = itFunctionFirstArgument.text;
-                            const exampleInfo: ExampleInfo = {
-                              logs: [],
-                              message: exampleMessage,
-                              statements: []
-                            }
-                            const itFunctionBlock = itFunctionSecondArgument.body as Block;
-                            if (itFunctionBlock.kind === 234) {
-                              for (let index = 0; index < itFunctionBlock.statements.length; index++) {
-                                const itFunctionBlockStatement = itFunctionBlock.statements[index] as ExpressionStatement;
-                                // If its a function call expression
-                                if (itFunctionBlockStatement.kind === 237) {
-                                  // Find the expect function call
-                                  const expectFunctionStatement = functionChecker(itFunctionBlockStatement, "expect");
-                                  if (expectFunctionStatement) {
-                                    const expectedValue = expectFunctionStatement[0].arguments[0];
-                                    exampleInfo.logs.push({
-                                      output: printer.printNode(ts.EmitHint.Unspecified, expectedValue, sourceFile),
-                                      arg: printer.printNode(ts.EmitHint.Unspecified, expectFunctionStatement[expectFunctionStatement.length - 1].arguments[0], sourceFile)
-                                    });
-                                  } else {
-                                    exampleInfo.statements.push(printer.printNode(ts.EmitHint.Unspecified, itFunctionBlockStatement, sourceFile))
-                                  }
-                                } else {
-                                  // Add all the extra block statements 
-                                  exampleInfo.statements.push(printer.printNode(ts.EmitHint.Unspecified, itFunctionBlockStatement, sourceFile))
-                                }
-                              }
-                              // Add the example info corresponding to the function name
-                              if (!functionExamplesRecord[functionName]) {
-                                functionExamplesRecord[functionName] = [
-                                  exampleInfo
-                                ]
-                              } else {
-                                functionExamplesRecord[functionName].push(exampleInfo)
-                              }
-                            }
-                          })
-                        }
-                      }
-                    }
-                  }
-                })
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  await traverse(testFilesDirectory);
-  return functionExamplesRecord
+	async function traverse(testFilesDirectory: string) {
+		const testFiles = await fs.readdir(testFilesDirectory);
+		for (let index = 0; index < testFiles.length; index++) {
+			const testFile = testFiles[index];
+			const testFilePath = path.join(testFilesDirectory, testFile);
+			const fsStat = await fs.stat(testFilePath);
+			if (fsStat.isDirectory()) {
+				// If its a directory, recursively call the function
+				await traverse(testFilePath);
+			} else {
+				// Make sure its a test file
+				if (testFile.endsWith('test.ts')) {
+					const program = ts.createProgram([testFilePath], {});
+					const sourceFile = program.getSourceFile(testFilePath)!;
+					const importedFunctions: Set<string> = new Set();
+					for (let index = 0; index < sourceFile.statements.length; index++) {
+						const statement = sourceFile.statements[index] as
+							| ExpressionStatement
+							| ImportDeclaration;
+						if (statement.kind === 265) {
+							if (statement.importClause) {
+								const namedImports = statement.importClause.namedBindings as NamedImportBindings;
+								if (namedImports.kind === 268) {
+									const importSpecifiers = namedImports.elements;
+									importSpecifiers.forEach((importSpecifier) => {
+										importedFunctions.add(importSpecifier.name.escapedText as string);
+									});
+								}
+							}
+						} else {
+							const describeFunctionStatement = functionChecker(statement, 'describe');
+							if (describeFunctionStatement) {
+								argumentsChecker(
+									describeFunctionStatement[0],
+									(describeFunctionFirstArgument, describeFunctionSecondArgument) => {
+										const functionName = describeFunctionFirstArgument.text;
+										// Make sure the function has been imported in the module
+										if (importedFunctions.has(functionName)) {
+											const describeFunctionBlock = describeFunctionSecondArgument.body as Block;
+											if (describeFunctionBlock.kind === 234) {
+												// Loop through all the function block statements
+												for (
+													let index = 0;
+													index < describeFunctionBlock.statements.length;
+													index++
+												) {
+													const describeFunctionBlockStatement = describeFunctionBlock.statements[
+														index
+													] as ExpressionStatement;
+													const itFunctionStatement = functionChecker(
+														describeFunctionBlockStatement,
+														'it'
+													);
+
+													if (itFunctionStatement) {
+														// Only if the first argument of it and describe are the same
+														argumentsChecker(
+															itFunctionStatement[0],
+															(itFunctionFirstArgument, itFunctionSecondArgument) => {
+																const exampleMessage = itFunctionFirstArgument.text;
+																const exampleInfo: ExampleInfo = {
+																	logs: [],
+																	message: exampleMessage,
+																	statements: [],
+																};
+																const itFunctionBlock = itFunctionSecondArgument.body as Block;
+																if (itFunctionBlock.kind === 234) {
+																	for (
+																		let index = 0;
+																		index < itFunctionBlock.statements.length;
+																		index++
+																	) {
+																		const itFunctionBlockStatement = itFunctionBlock.statements[
+																			index
+																		] as ExpressionStatement;
+																		// If its a function call expression
+																		if (itFunctionBlockStatement.kind === 237) {
+																			// Find the expect function call
+																			const expectFunctionStatement = functionChecker(
+																				itFunctionBlockStatement,
+																				'expect'
+																			);
+																			if (expectFunctionStatement) {
+																				const expectedValue =
+																					expectFunctionStatement[0].arguments[0];
+																				exampleInfo.logs.push({
+																					output: printer.printNode(
+																						ts.EmitHint.Unspecified,
+																						expectedValue,
+																						sourceFile
+																					),
+																					arg: printer.printNode(
+																						ts.EmitHint.Unspecified,
+																						expectFunctionStatement[
+																							expectFunctionStatement.length - 1
+																						].arguments[0],
+																						sourceFile
+																					),
+																				});
+																			} else {
+																				exampleInfo.statements.push(
+																					printer.printNode(
+																						ts.EmitHint.Unspecified,
+																						itFunctionBlockStatement,
+																						sourceFile
+																					)
+																				);
+																			}
+																		} else {
+																			// Add all the extra block statements
+																			exampleInfo.statements.push(
+																				printer.printNode(
+																					ts.EmitHint.Unspecified,
+																					itFunctionBlockStatement,
+																					sourceFile
+																				)
+																			);
+																		}
+																	}
+																	// Add the example info corresponding to the function name
+																	if (!functionExamplesRecord[functionName]) {
+																		functionExamplesRecord[functionName] = [exampleInfo];
+																	} else {
+																		functionExamplesRecord[functionName].push(exampleInfo);
+																	}
+																}
+															}
+														);
+													}
+												}
+											}
+										}
+									}
+								);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	await traverse(testFilesDirectory);
+	return functionExamplesRecord;
 }
--- libs/generateExamples.ts
+++ libs/generateExamples.ts
@@ -1,88 +1,119 @@
-import fs from "fs/promises";
-import { fromMarkdown } from "mdast-util-from-markdown";
-import { Heading, HTML } from "mdast-util-from-markdown/lib";
+import fs from 'fs/promises';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { Heading, HTML } from 'mdast-util-from-markdown/lib';
 import { frontmatterFromMarkdown, frontmatterToMarkdown } from 'mdast-util-frontmatter';
 import { gfmTableFromMarkdown, gfmTableToMarkdown } from 'mdast-util-gfm-table';
-import { toMarkdown } from "mdast-util-to-markdown";
+import { toMarkdown } from 'mdast-util-to-markdown';
 import { frontmatter } from 'micromark-extension-frontmatter';
 import { gfmTable } from 'micromark-extension-gfm-table';
-import { FunctionExampleRecord } from "./types";
+import { FunctionExampleRecord } from './types';
 
 function normalizeString(inputString: string) {
-  return inputString.replace(/`/g, "\\`").replace(/\\n/g, "\\\\n").split("\n").join("\\n")
+	return inputString.replace(/`/g, '\\`').replace(/\\n/g, '\\\\n').split('\n').join('\\n');
 }
 
-export async function generateExamples(moduleMarkdownPath: string, functionExamplesRecord: FunctionExampleRecord, packageName: string) {
-  const moduleMarkdownContent = await fs.readFile(moduleMarkdownPath, "utf-8");
-  const markdownTree = fromMarkdown(moduleMarkdownContent, {
-    extensions: [gfmTable, frontmatter(['toml', 'yaml'])],
-    mdastExtensions: [gfmTableFromMarkdown, frontmatterFromMarkdown(['yaml', 'toml'])]
-  });
+export async function generateExamples(
+	moduleMarkdownPath: string,
+	functionExamplesRecord: FunctionExampleRecord,
+	packageName: string
+) {
+	const moduleMarkdownContent = await fs.readFile(moduleMarkdownPath, 'utf-8');
+	const markdownTree = fromMarkdown(moduleMarkdownContent, {
+		extensions: [gfmTable, frontmatter(['toml', 'yaml'])],
+		mdastExtensions: [gfmTableFromMarkdown, frontmatterFromMarkdown(['yaml', 'toml'])],
+	});
 
-  markdownTree.children.splice(1, 0, {
-    type: "html",
-    value: `import CodeBlock from "@theme/CodeBlock";`
-  })
+	markdownTree.children.splice(1, 0, {
+		type: 'html',
+		value: `import CodeBlock from "@theme/CodeBlock";`,
+	});
 
-  // Skip first node since its the slug
-  for (let index = 1; index < markdownTree.children.length; index++) {
-    const markdownTreeChildren = markdownTree.children[index];
-    // Only h3 are used as function headings
-    if (markdownTreeChildren.type === "heading" && markdownTreeChildren.depth === 3) {
-      const [textChildNode] = markdownTreeChildren.children;
-      // Make sure the function has an example in the record
-      if (textChildNode.type === "text" && functionExamplesRecord[textChildNode.value]) {
-        for (let innerIndex = index + 1;; innerIndex++) {
-          const childNode = markdownTree.children[innerIndex];
-          if (childNode?.type === "heading" && childNode.depth === 4) {
-            if (childNode.children[0]?.type === "text" && childNode.children[0].value === "Defined in") {
-              const exampleInfos = functionExamplesRecord[textChildNode.value];
-              exampleInfos.forEach(({statements, logs, message}, exampleInfoIndex) => {
-                const exampleCode: string[] = [`import { ${textChildNode.value} } from "${packageName}";\\n`];
-                statements.forEach(statement => {
-                  exampleCode.push(normalizeString(statement))
-                })
-  
-                logs.forEach(({arg}) => {
-                  exampleCode.push(`console.log(${normalizeString(arg)});`)
-                })
-  
-                const codeExampleHtmlNode: HTML = {
-                  type: "html",
-                  value: [`##### ${message}`,`<div id="${textChildNode.value}-example-${exampleInfoIndex}" className="code-container">`, `\t<CodeBlock className="language-ts code-block">`, `{\`${exampleCode.join("\\n")}\`}`, "</CodeBlock>", `\t<CodeBlock className="language-ts code-block">`, `{\`${logs.map(({arg, output}) => `// ${normalizeString(arg)}\\n${normalizeString(output)}`).join("\\n")}\`}`, "</CodeBlock>", "</div>"].join("\n"),
-                }
-  
-                markdownTree.children.splice(innerIndex, 0, codeExampleHtmlNode);
-              })
+	// Skip first node since its the slug
+	for (let index = 1; index < markdownTree.children.length; index++) {
+		const markdownTreeChildren = markdownTree.children[index];
+		// Only h3 are used as function headings
+		if (markdownTreeChildren.type === 'heading' && markdownTreeChildren.depth === 3) {
+			const [textChildNode] = markdownTreeChildren.children;
+			// Make sure the function has an example in the record
+			if (textChildNode.type === 'text' && functionExamplesRecord[textChildNode.value]) {
+				for (let innerIndex = index + 1; ; innerIndex++) {
+					const childNode = markdownTree.children[innerIndex];
+					if (childNode?.type === 'heading' && childNode.depth === 4) {
+						if (
+							childNode.children[0]?.type === 'text' &&
+							childNode.children[0].value === 'Defined in'
+						) {
+							const exampleInfos = functionExamplesRecord[textChildNode.value];
+							exampleInfos.forEach(({ statements, logs, message }, exampleInfoIndex) => {
+								const exampleCode: string[] = [
+									`import { ${textChildNode.value} } from "${packageName}";\\n`,
+								];
+								statements.forEach((statement) => {
+									exampleCode.push(normalizeString(statement));
+								});
 
-              const headerNode: Heading = {
-                depth: 4,
-                type: "heading",
-                children: [
-                  {
-                    type: "text",
-                    value: "Example"
-                  }
-                ]
-              }
+								logs.forEach(({ arg }) => {
+									exampleCode.push(`console.log(${normalizeString(arg)});`);
+								});
 
-              markdownTree.children.splice(innerIndex, 0, headerNode);
-              // Set the index, to skip the visited nodes, along with the inserted ones
-              index = innerIndex + 1 + exampleInfos.length;
-              break;
-            }
-          } 
-        }
-      }
-    }
-  }
-  
-  // Add the slug with the transformed markdown tree
-  await fs.writeFile(moduleMarkdownPath, toMarkdown({
-    type: "root",
-    children: markdownTree.children
-  }, {
-    rule: "_",
-    extensions: [gfmTableToMarkdown(), frontmatterToMarkdown(['yaml', 'toml'])]
-  }), "utf-8")
+								const codeExampleHtmlNode: HTML = {
+									type: 'html',
+									value: [
+										`##### ${message}`,
+										`<div id="${textChildNode.value}-example-${exampleInfoIndex}" className="code-container">`,
+										`\t<CodeBlock className="language-ts code-block">`,
+										`{\`${exampleCode.join('\\n')}\`}`,
+										'</CodeBlock>',
+										`\t<CodeBlock className="language-ts code-block">`,
+										`{\`${logs
+											.map(
+												({ arg, output }) =>
+													`// ${normalizeString(arg)}\\n${normalizeString(output)}`
+											)
+											.join('\\n')}\`}`,
+										'</CodeBlock>',
+										'</div>',
+									].join('\n'),
+								};
+
+								markdownTree.children.splice(innerIndex, 0, codeExampleHtmlNode);
+							});
+
+							const headerNode: Heading = {
+								depth: 4,
+								type: 'heading',
+								children: [
+									{
+										type: 'text',
+										value: 'Example',
+									},
+								],
+							};
+
+							markdownTree.children.splice(innerIndex, 0, headerNode);
+							// Set the index, to skip the visited nodes, along with the inserted ones
+							index = innerIndex + 1 + exampleInfos.length;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Add the slug with the transformed markdown tree
+	await fs.writeFile(
+		moduleMarkdownPath,
+		toMarkdown(
+			{
+				type: 'root',
+				children: markdownTree.children,
+			},
+			{
+				rule: '_',
+				extensions: [gfmTableToMarkdown(), frontmatterToMarkdown(['yaml', 'toml'])],
+			}
+		),
+		'utf-8'
+	);
 }
